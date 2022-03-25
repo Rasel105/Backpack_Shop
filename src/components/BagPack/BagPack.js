e2779147c2c4eb0de006326efcf7d15d0eb91795
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-03-25 23:14:08 +0600

    onClick function added

--- src/components/BagPack/BagPack.js
+++ src/components/BagPack/BagPack.js
@@ -9,14 +9,17 @@ const BagPack = () => {
                .then(data => setBags(data))
      }, [])
      // console.log(bags)
+     const handleAddToCart = product => {
+          console.log(product)
+     }
      return (
           <div className='bag-card-container'>
                <div className='cag-card'>
                     {
-                         bags.map(bag => <Products key={bag.id} bag={bag} />)
+                         bags.map(bag => <Products key={bag.id} bag={bag} handleAddToCart={handleAddToCart} />)
                     }
                </div>
-               <div>
+               <div className='add-to-cart-container'>
                     <h1>Right side</h1>
                </div>
           </div>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Products.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-const Products = ({ bag }) => {
+const Products = ({ bag, handleAddToCart }) => {
      const { image, price, name } = bag
-     console.log(bag)
+     // console.log(handleAddToCart)
      return (
           <div className='product'>
                <img src={image} alt="" />
@@ -12,7 +12,7 @@ const Products = ({ bag }) => {
                     <h4>Name: {name}</h4>
                     <p>Price: $ {price}</p>
                </div>
-               <button className='add-to-cart'>
+               <button onClick={handleAddToCart(bag)} className='add-to-cart'>
                     <p>Add to Cart</p>
                     <FontAwesomeIcon icon={faShoppingCart} size="xl" />
                </button>
