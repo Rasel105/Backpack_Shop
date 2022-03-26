72c79957f9bbec4539603c6ced2bc605f43fd925
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-03-26 15:15:30 +0600

    queastion ans done

--- src/QuesAns/QuesAns.js
+++ src/QuesAns/QuesAns.js
@@ -4,11 +4,18 @@ const QuesAns = () => {
      return (
           <div className='ques-ans'>
                <h3>1. How React Works?</h3>
-               <h3>2. What is the difference between Props and State?</h3>
                <p>
                     <h3>Ans: </h3>
-                    React is a Javascript Lirabray for building user Interface. First think is. React.js works one way data binding,
+                    React is a Javascript Lirabray for building user Interface. First think is. React.js works one way data binding, when ever any changes have in the web application, the hold ul re-render in the Virtual DOM, Virtual DOM the copy of Present DOM, when changes happen, the it check previous DOM And Current DOM, both two DOM compare where the changes happened, the it just update where the changes. that's why React is so Fast. This is how React Works.
                </p>
+               <h3>2. How useState Works?</h3>
+               <p>
+                    <h3>Ans: </h3>
+                    The useState is the heart of React components, useState() it is just for use contain information about the components, the useState is can be changes any time by the user or system interactaction.  the useState is updabled. the useState is also called statefull component. useState is access or modified inside the component. we have set and initial value before any interactaction with useState().
+               </p>
+
+               <h3>3. What is the difference between Props and State?</h3>
+
                <p>
                     <h4>Ans: Props: </h4>
                     1. Props are read only.
